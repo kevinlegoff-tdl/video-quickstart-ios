bcd676efc1acb928136423b74db13c755bf053c9
Kevin LE GOFF <kevin.le.goff@testdevlab.com>
2023-09-14 13:08:14 +0300

    Add PIP example project

---
+++ PiPExample/PiPExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PiPExample
+//
+//  Created by Kevin LE GOFF on 14/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
